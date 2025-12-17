cb824ab8317fe4bc644bd41fa999426ea5750812
Dhruv Mehta <dhruv.mehta@dream11.com>
2025-12-17 23:08:35 +0530

    Add type annotations and structure to configuration constants
    - Introduced TypedDicts for MysqlDBConfig and ConfigMap to enhance type safety and clarity in configuration management.
    - Updated CONFIG_MAP to use the new ConfigMap type, ensuring consistent type annotations in the Config class.
    - Improved return type annotations for properties in the Config class for better type clarity.

--- mlflow/app_layer/src/mlflow_app_layer/constant/config.py
+++ mlflow/app_layer/src/mlflow_app_layer/constant/config.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from typeguard import typechecked
-from mlflow_app_layer.config.constants import CONFIG_MAP
+from mlflow_app_layer.config.constants import CONFIG_MAP, ConfigMap
 
 
 @typechecked
@@ -11,19 +11,19 @@ class Config:
     """
 
     def __init__(self):
-        self._config = CONFIG_MAP
+        self._config: ConfigMap = CONFIG_MAP
         print(f"Config: {self._config}")
 
     @property
-    def mlflow_ui_url(self):
+    def mlflow_ui_url(self) -> str:
         return self._config["MLFLOW_UI_URL"]
 
     @property
-    def mlflow_app_layer_url(self):
+    def mlflow_app_layer_url(self) -> str:
         return self._config["MLFLOW_APP_LAYER_URL"]
 
     @property
-    def mlflow_app_base_path(self):
+    def mlflow_app_base_path(self) -> str:
         return self._config["MLFLOW_APP_BASE_PATH"]
 
     def mlflow_admin_credentials(self) -> dict[str, str]:
@@ -33,11 +33,11 @@ class Config:
         }
 
     def db_config(self) -> dict[str, str]:
-        mysql_db = self._config["mysql_db"]  # type: ignore[index]
+        mysql_db = self._config["mysql_db"]
         return {
-            "host": mysql_db["host"],  # type: ignore[index]
-            "user": mysql_db["username"],  # type: ignore[index]
-            "password": mysql_db["password"],  # type: ignore[index]
-            "database": mysql_db["database"],  # type: ignore[index]
-            "port": mysql_db["port"],  # type: ignore[index]
+            "host": mysql_db["host"],
+            "user": mysql_db["username"],
+            "password": mysql_db["password"],
+            "database": mysql_db["database"],
+            "port": mysql_db["port"],
         }
